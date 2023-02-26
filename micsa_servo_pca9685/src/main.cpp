2aa975501608cae27b8911a76e24bb26d6498011
inaciose <inaciose@gmail.com>
2023-02-26 21:06:54 +0000

    door sensor added

--- micsa_servo_pca9685/src/main.cpp
+++ micsa_servo_pca9685/src/main.cpp
@@ -6,22 +6,26 @@
   #define LED_BUILTIN PC13
 #endif
 
+//unsigned int ledTimerLast = 0;
+//int ledTimerTrigger = 500;
+
+// door sensor switch
+#define BUTTON_PIN1 PA0
+int buttonState1;
+int buttonLastState1;
+
+// setup PWMServoDriver with I2C
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41); // select address
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire); // select address & chanell 
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  250 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  500 // This is the 'maximum' pulse length count (out of 4096)
 #define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
 #define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
@@ -35,71 +39,57 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(SERIAL_BAUDS);
   delay(2000);
+
+  // door sensor button setup 
+  pinMode(BUTTON_PIN1, INPUT);
+
  // initialize PCA9685
-  Serial.println("8 channel Servo test!");
+  Serial.println("initialize PCA9685");
   pwm.begin();
-  Serial.println("pwm.begin");
+  //Serial.println("pwm.begin");
   pwm.setOscillatorFrequency(27000000);
-  Serial.println("pwm.setOscillatorFrequency");
+  //Serial.println("pwm.setOscillatorFrequency");
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
-  Serial.println("pwm.setPWMFreq");
-  delay(2000);
+  //Serial.println("pwm.setPWMFreq");
+  delay(10);
+ 
 }
 
-/*
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000000;  // convert input seconds to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-}
-*/
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("LED on");
-
-  // Drive each servo one at a time using setPWM()
-  Serial.println(servonum);
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, pulselen);
-  }
-
-  delay(500);
-  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, pulselen);
-  }
-
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("LED off");
   
-  delay(500);
 
-/*
-  // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
-  // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior. 
-  for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
-    pwm.writeMicroseconds(servonum, microsec);
+  // door sensor reading pooling no debounce
+  buttonState1 = digitalRead(BUTTON_PIN1);
+  digitalWrite(LED_BUILTIN, buttonState1);
+
+  Serial.print(buttonState1);
+  if(buttonState1) {
+    Serial.println(" max");
+    //pwm.setPWM(servonum, 0, SERVOMAX);
+    if(buttonState1 != buttonLastState1) {
+    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+      pwm.setPWM(servonum, 0, pulselen);
+    }
+    buttonLastState1 = buttonState1;
+    }
+    
+  } else {
+    Serial.println(" min");
+    //pwm.setPWM(servonum, 0, SERVOMIN);
+    if(buttonState1 != buttonLastState1) {
+      for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+        pwm.setPWM(servonum, 0, pulselen);
+      }
+      buttonLastState1 = buttonState1;
+    }
   }
-
-  delay(500);
-  for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
+  
+  /*
+  if((millis() - ledTimerLast) > ledTimerTrigger {
+    ledTimerLast = millis();
+    digitalWrite(LED_PIN1, buttonState1);
   }
-*/
-  delay(500);
-
-  servonum++;
-  if (servonum > 0) servonum = 0; // Testing the first 8 servo channels
+  */
 }
